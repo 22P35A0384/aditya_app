514b9e663425c04a9ed5766df018b9d9fc9117fa
22P35A0384 <sai.gangadhar2022@outlook.com>
2024-03-16 03:05:22 +0530

    16/03/2024 03:05 AM

--- app/components/spotlight.js
+++ app/components/spotlight.js
@@ -1,13 +1,78 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Styles from '../styles/spotlightstyles.js';
-import { Text, View,Image,Pressable } from "react-native";
+import { Text, View,Image,Pressable, Button } from "react-native";
 import { router } from "expo-router";
+import { Camera,CameraType } from "expo-camera";
+import * as MediaLibrary from 'expo-media-library';
+import { TouchableOpacity } from "react-native-gesture-handler";
+import {Entypo} from '@expo/vector-icons'
+import Buttons from "./buttion.js";
 
 const Spotlight=()=>{
+    const [hasCamerapermission, sethasCemarapermission] = useState(null);
+    const [image, setimage] = useState(null);
+    const [type,setType] = useState(Camera.Constants.Type.back);
+    const [flash,setFlash] = useState(Camera.Constants.FlashMode.off);
+    const cameraRef = useRef(null);
+
+    useEffect(()=>{
+        (async ()=>{
+            MediaLibrary.requestPermissionsAsync();
+            const cameraStatus = await Camera.requestCameraPermissionsAsync();
+            Camera.requestCameraPermissionsAsync()
+            sethasCemarapermission(cameraStatus.status === 'granted');
+        })();
+    },[])
+
+    const takePicture = async()=>{
+        if(cameraRef){
+            try{
+                const data = await cameraRef.current.takePictureAsync();
+                console.log(data)
+                setimage(data.uri)
+            }catch(err){
+                console.log(err)
+            }
+        }
+    }
+
+    if(hasCamerapermission === false){
+        return <Text>Camera Access Denied</Text>
+    }
     return(
         <>
             <View style={Styles.container}>
-                <Text>Spotlight Page</Text>
+                <View style={Styles.sub}>
+                    <Text style={Styles.head}>ADITYA UNIVERSITY</Text>
+                    <Image
+                    source={require('../../public/logo.png')}
+                    style={Styles.img1}
+                    />
+                </View>
+                <View style={Styles.sub2}>
+                    {/* <Text>Spotlight Block</Text> */}
+                    <Camera
+                        style={Styles.camera}
+                        type={type}
+                        flashMode={flash}
+                        ref={cameraRef}
+                    >
+                        <Text></Text>
+                    </Camera>
+                </View>
+                <View style={Styles.sub3}>
+                    <Buttons tittle={'Get Student Details'} icon='camera' onPress={takePicture}/> 
+                </View>
+                <View style={Styles.sub4}>
+                    {!image ? 
+                    <></> 
+                    :
+                    <Image
+                        source={{uri:image}}
+                        style={{width:100,height:100}}
+                    />   
+                }
+                </View>
             </View>
             <View style={Styles.sub1}>
                 <Pressable
